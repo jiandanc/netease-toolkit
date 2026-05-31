09404e2a9a268b9d65e4fbf02da2ec0c4a09a45a
jiandanc <jiandanc@126.com>
2026-05-31 14:28:50 +0800

    delete scripts

--- scripts/write_convert.py
+++
@@ -1,5 +0,0 @@
-﻿import pathlib,base64,sys
-
-data = sys.stdin.buffer.read()
-pathlib.Path(sys.argv[1]).write_bytes(base64.b64decode(data))
-print('OK')
--- scripts/write_helper.py
+++
@@ -1,6 +0,0 @@
-import sys
-import pathlib
-p = sys.argv[1]
-content = sys.stdin.read()
-pathlib.Path(p).write_text(content, encoding="utf-8")
-print("OK")
